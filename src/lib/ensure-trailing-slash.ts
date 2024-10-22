14d88846f1ba4eb118c6e2908ecf98668d621783
Stefan Probst <stefanprobst@posteo.de>
2024-10-22 20:24:39 +0200

    chore: lint

--- src/lib/ensure-trailing-slash.ts
+++ src/lib/ensure-trailing-slash.ts
@@ -1,5 +1,5 @@
 export function ensureTrailingSlash(path: string): string {
 	if (path.split("/").at(-1)?.includes(".")) return path;
 
-	return path.endsWith("/") ? path : path + "/";
+	return path.endsWith("/") ? path : `${path}/`;
 }
